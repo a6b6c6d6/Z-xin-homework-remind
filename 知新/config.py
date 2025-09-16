ebb1bfb72d8df0d0ffe7e212c469e8017e40d036
fuyao <2895653968@qq.com>
2025-09-16 21:30:44 +0800

    Update config.py

--- 知新/config.py
+++ 知新/config.py
@@ -1,9 +1,11 @@
 import os         
-TOKEN = os.getenv("ZXIN_TOKEN")  
+from gettoken import gettoken
+TOKEN = gettoken()
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0",
     "zxin-ucenter-token": TOKEN,
 }
 URL_CLASSROOM = "https://v3.api.z-xin.net/classroom/studentGetClassroom"
 URL_HOMEWORK_PAGE = "https://v3.api.z-xin.net/homework/studentGetHomeworkPage"
+
 URL_HOMEWORK_DETAIL = "https://v3.api.z-xin.net/homework/getHomeworkDetail"
