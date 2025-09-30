861edc20210c7f9ca9ca1cfcc7de3560e3077344
fuyao <2895653968@qq.com>
2025-09-30 20:56:19 +0800

    Update helper.py
    对answerProgress进行判断==0才是未完成作业

--- 知新/helper.py
+++ 知新/helper.py
@@ -5,9 +5,10 @@ from dingtalk import send_ding
 def get_hw_count(list, aaa):
     return len(list[aaa])
 
-def judge(unfinishCount_i, deadline, msg, courseName_i):
-    if within_48h(deadline) == -1 and unfinishCount_i != 0 and overdeadline(deadline) != -1:
+def judge(unfinishCount_i, deadline, msg, courseName_i,answerProgress):
+    if answerProgress==0 and within_48h(deadline) == -1 and unfinishCount_i != 0 and overdeadline(deadline) != -1:
         print(msg)          # 保留终端输出
         send_ding(msg)      # 新增钉钉提醒
     else:
+
         print(f"课程《{courseName_i}》暂无48小时内截止的作业或均已完成")
